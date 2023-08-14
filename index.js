eff5362398d4db104f472cd4e376658962744b4c
silvaroger <rs.rogerrafael@gmail.com>
2023-08-14 19:31:45 -0300

    create first class

---
+++ index.js
@@ -0,0 +1,12 @@
+class Cliente {
+  nome
+  cpf
+  agencia
+  saldo
+}
+
+const cliente1 = new Cliente()
+cliente1.nome = "Róger"
+cliente1.cpf = 11122233309;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
