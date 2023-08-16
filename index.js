ef5f8a3badb777fd8fbf8f0b9f0c8fdeffcedb18
silvaroger <rs.rogerrafael@gmail.com>
2023-08-16 20:49:34 -0300

    create method 'sacar' and 'depositar'

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 class Cliente {
   nome
   cpf
+}
+
+class ContaCorrente {
   agencia
-  saldo
+  _saldo = 0;
+
+  sacar(valor) {
+    if(this._saldo >= valor){
+      this._saldo -= valor;
+      return valor;
+    }
+
+    depositar(valor){
+      if(valor > 0){
+        this._saldo += 0;
+      }
+    }
+  }
+
 }
 
 const cliente1 = new Cliente()
 cliente1.nome = "Róger"
-cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
+cliente1.cpf = 11122233309
+cliente1.agencia = 1001
+cliente1.saldo = 0
