450730a02bbec933a6269d963ca4332c457effdb
silvaroger <rs.rogerrafael@gmail.com>
2023-08-16 22:10:48 -0300

    separado as classes em arquivos individuais

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
-class Cliente {
-  nome
-  cpf
-}
-
-class ContaCorrente {
-  agencia
-  _saldo = 0;
-
-  sacar(valor) {
-    if(this._saldo >= valor){
-      this._saldo -= valor;
-      return valor;
-    }
-
-    depositar(valor){
-      if(valor > 0){
-        this._saldo += 0;
-      }
-    }
-  }
-
-}
+import { Cliente } from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
 
 const cliente1 = new Cliente()
 cliente1.nome = "Róger"
 cliente1.cpf = 11122233309
-cliente1.agencia = 1001
-cliente1.saldo = 0
+
+const cliente2 = new Cliente();
+cliente2.nome = 'Aline';
+cliente2.cpf = 88844455509;
+
+const contaCorrenteRicardo = new ContaCorrente;
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+
+const conta2 = new ContaCorrente;
+conta2.agencia = 1002;
+conta2.cliente = cliente2;
+
+
+console.log(conta2);
